730416e28ff80cada7f85e379dedb64c506cb5ea
bishalthapa2055 <bishalthapa2055@gmail.com>
2023-12-21 15:35:23 +0545

    added react-modal

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,11 +1,26 @@
 import { useRouter } from 'next/router'
 import React from 'react'
+import Modal from 'react-modal';
+
+Modal.setAppElement('#__next ');
 
 const Video = () => {
     const router = useRouter()
     console.log(router.query.videoId)
   return (
+    <>
     <div>VideoId : {router.query.videoId}</div>
+    <div>
+    <Modal
+      isOpen={true}
+      contentLabel='Watch the video'
+      onRequestClose={() => router.back()}
+    >
+     <h1> here is modal </h1>
+     
+    </Modal>
+  </div>
+    </>
   )
 }
 
