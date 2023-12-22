83643dfef12b7bc30d5d856114b3137bfff032be
bishalthapa2055 <bishalthapa2055@gmail.com>
2023-12-22 15:20:04 +0545

    :dart: stuing modal :dart:

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -8,6 +8,14 @@ Modal.setAppElement('#__next ');
 const Video = () => {
     const router = useRouter()
     console.log(router.query.videoId)
+
+    const video = {
+      title :"Hi title",
+      publishTime :"1990-90-09",
+      description :" Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      channelTitle :"Paramount Pictures",
+      viewsCount : 100090
+    }
   return (
     <>
     <div className={styles.container}>
@@ -25,7 +33,36 @@ const Video = () => {
       frameborder="0"
       className={styles.videoplayer}
       ></iframe>
-      <h1> here is modal </h1>
+      <div className={styles.mainModal}>
+
+      <div className={styles.modalBody}>
+        <div className={styles.modalBodyLeft}>
+          <div className={styles.publishTime}>
+            <h4 className={styles.publishTimeModal}> {video.publishTime}</h4>
+          </div>
+          <div className={styles.title}>
+            <h2>{video.title}</h2>
+          </div>
+          <div className={styles.description}>
+            <h3 className={styles.stylesDescription}>{video.description}</h3>
+          </div>
+        </div>
+        <div className={styles.modalBodyRight}>
+          <div className={styles.castName}>
+            <p className={styles.subText}>
+              <span className={styles.infoTextKey}>Cast : </span>
+              <span className={styles.channelTitle}>{video.channelTitle}</span>
+            </p>
+          </div>
+          <div className={styles.viewsCount}>
+             <p className={styles.subText}>
+              <span className={styles.infoTextKey}>ViewsCount : </span>
+              <span className={styles.channelTitle}> {video.viewsCount}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      </div>
     </div>
      
     </Modal>
